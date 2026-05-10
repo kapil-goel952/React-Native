faa750c9283397ecfa085ce7f1264425626a7d07
kapil-goel952 <goelk849@gmail.com>
2026-05-11 01:41:20 +0530

    added the buttons interface

--- RNproject01/app/(tabs)/index.tsx
+++ RNproject01/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View,ScrollView, Text, Image,ImageBackground, Dimensions } from "react-native";
+import { Pressable, View, ScrollView, Text, Image, ImageBackground, Button, Dimensions } from "react-native";
 
 const { width, height } = Dimensions.get("window");
 
@@ -14,114 +14,147 @@ export default function HomeScreen() {
         // height: width,  // swapped
         // transform: [{ rotate: "90deg" }],
         // position: "absolute",
-        flex:1,
+        flex: 1,
         // top: (height - width) / 2,
         // left: -(height - width) / 2,
       }}
     >
-      <ScrollView
-        style={{
-          flex: 1,
-          margin:12,
-
-          paddingTop:30,
-          paddingBottom:20,
-          paddingRight:20,
-          paddingLeft:20,
-          // justifyContent: "center",
-          // alignItems: "center",
-            // transform: [{ rotate: "-90deg" }],
-        }}
-      >
-        <Text
+      <View style={{
+        flex:1,
+      }}>
+        <ScrollView
           style={{
-            fontSize: 15,
-            color: "black",
-            margin:20
-          }}
-        >
-          sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
-        </Text>
-        <View><Image source={{uri:"https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}} 
-        style={{
-          height:100,width:150,margin:20
-          // position:"absolute"
-        }}></Image></View>
-        <Text
-          style={{
-            fontSize: 15,            color: "black",
-
-            margin:20
-          }}
-        >
+            flex: 1,
+            margin: 12,
+            paddingTop: 30,
+            paddingBottom: 20,
+            paddingRight: 20,
+            paddingLeft: 20,
           
-          sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
-        </Text>
-          <View><Image source={{uri:"https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}} 
-        style={{
-          height:100,width:150,margin:20
-          // position:"absolute"
-        }}></Image></View>
-        <Text
-          style={{
-            fontSize: 15,            color: "black",
-
-            margin:20
+            // justifyContent: "center",
+            // alignItems: "center",
+            // transform: [{ rotate: "-90deg" }],
           }}
         >
-          sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
-        </Text>
-          <View><Image source={{uri:"https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}} 
-        style={{
-          height:100,width:150,margin:20
-          // position:"absolute"
-        }}></Image></View>
-        <Text
-          style={{
-            fontSize: 15,            color: "black",
+          <Text
+            style={{
+              fontSize: 15,
+              color: "black",
+              margin: 20
+            }}
+          >
+            sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
+          </Text>
+          <View><Image source={{ uri: "https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }}
+            style={{
+              height: 100, width: 150, margin: 20
+              // position:"absolute"
+            }}></Image>
+            <Button
+              title="click me"
+              onPress={() => {
+                console.log("buton chal gyaaa")
 
-            margin:20
-          }}
-        >
-          sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
-        </Text>
-          <View><Image source={{uri:"https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}} 
-        style={{
-          height:100,width:150,margin:20
-          // position:"absolute"
-        }}></Image></View>
-        <Text
-          style={{
-            fontSize: 15,            color: "black",
+              }} />
 
-            margin:20
-          }}
-        >
-          sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
-        </Text>
-          <View><Image source={{uri:"https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}} 
-        style={{
-          height:100,width:150,margin:20
-          // position:"absolute"
-        }}></Image></View>
-        <Text
-          style={{
-            fontSize: 15,            color: "black",
+          </View>
+          <Text
+            style={{
+              fontSize: 15,
+              color: "black",
+              margin: 20
+            }}
+          >
 
-            margin:20
-          }}
-        >
-          sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
-        </Text>
-          <View><Image source={{uri:"https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}} 
-        style={{
-          height:100,width:150,margin:20
-          // position:"absolute"
-        }}></Image></View>
-      </ScrollView>
+            sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
+          </Text>
+          <View><Image source={{ uri: "https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }}
+            style={{
+              height: 100, width: 150, margin: 20
+              // position:"absolute"
+            }}></Image>
+            <Button title="click me" onPress={() => { console.log("buton chal gyaaa") }} />
+          </View>
+          <Text
+            style={{
+              fontSize: 15,
+              color: "black",
+              margin: 20
+            }}
+          >
+            sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
+          </Text>
+          <View><Image source={{ uri: "https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }}
+            style={{
+              height: 100, width: 150, margin: 20
+              // position:"absolute"
+            }}></Image>
+            <Button title="click me" onPress={() => { console.log("buton chal gyaaa") }} />
+          </View>
+          <Text
+            style={{
+              fontSize: 15,
+              color: "black",
+              margin: 20
+            }}
+          >
+            sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
+          </Text>
+          <View><Image source={{ uri: "https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }}
+            style={{
+              height: 100, width: 150, margin: 20
+              // position:"absolute"
+            }}></Image>
+            <Button title="click me" onPress={() => { console.log("buton chal gyaaa") }} />
+          </View>
+          <Text
+            style={{
+              fontSize: 15,
+              color: "black",
+              margin: 20
+            }}
+          >
+            sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
+          </Text>
+          <View><Image source={{ uri: "https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }}
+            style={{
+              height: 100, width: 150, margin: 20
+              // position:"absolute"
+            }}></Image>
+            <Button title="click me" onPress={() => { console.log("buton chal gyaaa") }} />
+
+          </View>
+          <Pressable onPress={() => console.log("text clickeddd......")
+          }>
+            <Text style={{
+              fontSize: 15,
+              color: "black",
+              margin: 20
+            }}>
+              trial of Pressable
+            </Text>
+          </Pressable>
+          <Text
+            style={{
+              fontSize: 15,
+              color: "black",
+              margin: 20
+            }}
+          >
+            sooooojaa Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque delectus numquam qui, amet quos architecto provident perspiciatis nam molestias voluptatum harum veritatis odit omnis impedit exercitationem expedita velit nihil hic.
+          </Text>
+          <View><Image source={{ uri: "https://images.unsplash.com/photo-1617531653332-bd46c24f2068?q=80&w=1515&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }}
+            style={{
+              height: 100, width: 150, margin: 20
+              // position:"absolute"
+            }}></Image>
+            <Button title="click me" onPress={() => { console.log("buton chal gyaaa") }} />
+          </View>
+
+        </ScrollView>
+      </View>
     </ImageBackground>
   );
 }
 
 
-{}
